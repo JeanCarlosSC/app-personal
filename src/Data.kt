9b4ce139e9ec427cdae87ea708a79e4b68856d89
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-08-23 21:07:47 -0500

    added persistence system

--- src/Data.kt
+++ src/Data.kt
@@ -1,17 +1,54 @@
+import java.io.*
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-val ITN = Asignatura(0, 1, 2)
-
-val inicio = LocalDate.of(2021, 8, 23)
-val hoy = LocalDate.now()
+val inicio: LocalDate = LocalDate.of(2021, 8, 23)
+val hoy: LocalDate = LocalDate.now()
 val numeroDeSemana = (ChronoUnit.DAYS.between(inicio, hoy)).toInt()/7 + 1
 
-class Asignatura(val autonomo: Int, val creditos: Int, val horasDeClase: Int) {
-    fun getHorasAutonomas(): Int {
+class Data {
+    val asignaturas = mutableListOf<Asignatura>()
+
+    init {
+        //informacion por defecto
+        asignaturas.add(Asignatura("Análisis de sistemas", 0, 2, 4))
+        asignaturas.add(Asignatura("Probabilidad", 0, 3, 4))
+        asignaturas.add(Asignatura("Expresión musical secuencializada", 0, 1, 2))
+        asignaturas.add(Asignatura("Introducción a la teoría de números", 0, 1, 2))
+        asignaturas.add(Asignatura("Ciencias de la computación 3", 0, 1, 2))
+        asignaturas.add(Asignatura("Segunda lengua 1", 0, 2, 4))
+        asignaturas.add(Asignatura("Liderazgo y desarrollo profesional", 0, 2, 4))
+
+        load()
+    }
+
+    fun load() {
+        val path = File("data/autonomo.ser")
+        if(path.exists() && path.isFile) {
+            val entrada = ObjectInputStream(FileInputStream("data/autonomo.ser"))
+            for (i in 0 until 7) {
+                asignaturas[i].autonomo = entrada.readObject() as Int
+            }
+        }
+    }
+
+    fun save() {
+        val salida = ObjectOutputStream(FileOutputStream("data/autonomo.ser"))
+        for(i in 0 until 7) {
+            salida.writeObject(asignaturas[i].autonomo)
+        }
+    }
+}
+
+class Asignatura(private val nombre: String, var autonomo: Int, private val creditos: Int, private val horasDeClase: Int) {
+    private fun getHorasAutonomas(): Int {
         return creditos*3 - horasDeClase
     }
-    fun getHorasPendientes(): Int {
+    private fun getHorasPendientes(): Int {
         return numeroDeSemana*getHorasAutonomas() - autonomo
+    }
+    override fun toString(): String {
+        val horasPendientes = getHorasPendientes()
+        return "$nombre: $horasPendientes hora${if(horasPendientes != 1)"s" else ""} pendiente${if(horasPendientes != 1)"s" else ""}."
     }
 }
--- src/ui/App.kt
+++ src/ui/App.kt
@@ -1,28 +1,49 @@
 package ui
 
-import ITN
+import Data
+import numeroDeSemana
 import javax.swing.JFrame
 import javax.swing.JLabel
 import java.time.LocalDate.now
+import javax.swing.JButton
 
 class App: JFrame() {
     init {
+        //load data
+        val data = Data()
+
+        //initial frame properties
         extendedState = MAXIMIZED_BOTH
         setSize(1320, 704)
 
+        //header
         val lFecha = JLabel()
-        lFecha.text = "${now()}"
+        lFecha.text = "${now()}: semana $numeroDeSemana."
         lFecha.horizontalAlignment = JLabel.CENTER
         lFecha.setSize(500, 32)
         lFecha.setLocation(width/2-lFecha.width/2, 32)
         add(lFecha)
 
-        val lITN = JLabel("Introducción a la teoría de números, autónomo: ${ITN.getHorasAutonomas()} restante: ${ITN.getHorasPendientes()}")
-        lITN.horizontalAlignment = JLabel.CENTER
-        lITN.setSize(500, 32)
-        lITN.setLocation(width/2-lITN.width/2, 64)
-        add(lITN)
+        //asignaturas
+        for (i in 0 until data.asignaturas.size) {
+            val lAsignatura = JLabel("${data.asignaturas[i]}")
+            lAsignatura.setSize(500, 32)
+            lAsignatura.setLocation(500, 64+i*32)
+            add(lAsignatura)
 
+            val btPlus = JButton("+")
+            btPlus.addActionListener {
+                data.asignaturas[i].autonomo ++
+                lAsignatura.text = data.asignaturas[i].toString()
+                repaint()
+                data.save()
+            }
+            btPlus.setSize(28, 28)
+            btPlus.setLocation(460, 66+i*32)
+            add(btPlus)
+        }
+
+        //final frame properties
         layout = null
         setLocationRelativeTo(this)
         defaultCloseOperation = EXIT_ON_CLOSE
