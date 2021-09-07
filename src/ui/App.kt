f3aee5e2bb9e7c92b363ea29ead16758070e0f1e
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-09-06 19:28:39 -0500

    update UI

--- src/ui/App.kt
+++ src/ui/App.kt
@@ -1,18 +1,85 @@
 package ui
 
 import Data
+import lib.sRAD.kotlin.gui.component.VentanaEmergente
 import lib.sRAD.kotlin.gui.sComponent.*
 import numeroDeSemana
 import javax.swing.JLabel
 import java.time.LocalDate.now
-import javax.swing.JOptionPane
 
 class App: SFrame(1320, 704) {
     //ui
-    private val pPrincipal = SPanel(196, 64, 900, 500, SPanel.EXTERNO)
+    private val pPrincipal = SPanel(196, 64, 900, 532, SPanel.EXTERNO)
     //components
     private val pResumen = SPanel(2, 2, 896, 496)
-    private val pListas = SPanel(2, 2, 896, 496)
+    private val pListas = object: SPanel(2, 2, 896, 496) {
+        val lLista: SLabel
+        val cListas: SComboBox
+        val scroll: SScrollPane
+        val pTasks: SPanel
+        val bAdd: SButton
+
+        init {
+            lLista = SLabel(34, 32, 68, text="Lista")
+            add(lLista)
+
+            cListas = SComboBox(132, 32, 200, 32, Data.getListNames())
+            add(cListas)
+
+            pTasks = SPanel(2, 2, 594, 400)
+
+            val wNewTask = object: VentanaEmergente(this@App, 500, 256) {
+                init {
+                    val lTask = SLabel(32, 32, 150, text = "Ingrese tarea")
+                    add(lTask)
+
+                    val taTask = STextArea(32, 94, 436, 68)
+                    add(taTask)
+
+                    val bAdd = SButton(134, 192, text = "Añadir")
+                    bAdd.addActionListener {
+                        Data.newTask(cListas.selectedIndex, taTask.text)
+                        cerrar()
+                        taTask.text = ""
+                        updateContent()
+                    }
+                    add(bAdd)
+
+                    val bClose = SButton(266, 192, text = "Cerrar")
+                    bClose.addActionListener { cerrar() }
+                    add(bClose)
+                }
+            }
+
+            bAdd = SButton(364, 32, text = "+ Tarea")
+            bAdd.addActionListener { wNewTask.lanzar() }
+            add(bAdd)
+
+            updateContent()
+
+            scroll = SScrollPane(32, 96, 600, 400, pTasks)
+            add(scroll)
+        }
+
+        fun updateContent() {
+            pTasks.removeAll()
+
+            val lista = Data.listas[cListas.selectedIndex].items
+            for (i in 0 until lista.size) {
+                val lTask = SLabel(32, 32+i*32, 500, text = lista[i])
+                pTasks.add(lTask)
+            }
+            if(64+lista.size*32>396) {
+                pTasks.setSize(pTasks.width, 64+lista.size*32)
+            }
+            else {
+                pTasks.setSize(pTasks.width, 396)
+            }
+
+            //repaint
+            pTasks.repaint()
+        }
+    }
 
     init {
         //frame properties
@@ -41,7 +108,6 @@ class App: SFrame(1320, 704) {
 
     private fun initComponents() {
         initPResumen()
-        initPListas()
         initPPrincipal()
     }
 
@@ -50,29 +116,6 @@ class App: SFrame(1320, 704) {
         val lFecha = SLabel(32, 32, 700, 32, "${now()}: semana $numeroDeSemana.")
         lFecha.horizontalAlignment = JLabel.CENTER
         pResumen.add(lFecha)
-
-        //asignaturas
-        for (i in 0 until Data.asignaturas.size) {
-            val lAsignatura = SLabel(96, 64 + 32*i, 600, 32, "${Data.asignaturas[i]}")
-            pResumen.add(lAsignatura)
-
-            val btPlus = SButton(32, 66 + 32*i, 28, 28, "+")
-            btPlus.addActionListener {
-                Data.asignaturas[i].autonomo++
-                lAsignatura.text = Data.asignaturas[i].toString()
-                repaint()
-                Data.save()
-            }
-            pResumen.add(btPlus)
-        }
-    }
-
-    private fun initPListas() {
-        val lLista = SLabel(34, 32, 100, text="Lista")
-        pListas.add(lLista)
-
-        val cListas = SComboBox(164, 32, 150, 32, Data.getListNames())
-        pListas.add(cListas)
     }
 
     private fun initPPrincipal() {
